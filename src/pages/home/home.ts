c80db170abbd46c0decf43e890c7e3988523b39b
James West <james.west@westdarley.co.uk>
2017-10-11 10:46:35 +0100

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,28 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  fields: { sizes: number[] }[] = [];
 
+  current = '';
+
+  constructor(public navCtrl: NavController) {
+    this.fields.push({ sizes: [1, 3, 4, 2, 2] });
+    this.fields.push({ sizes: [4, 4, 4] });
+    this.fields.push({ sizes: [3, 3, 3, 3] });
   }
+
+  add() {
+    const sizes = this.current.split(',').map(s => parseInt(s));
+
+    if (sizes.reduce((a, b) => a + b) !== 12) {
+      alert('you need it to equal 12');
+    }
+    else {
+      this.fields.push({ sizes: sizes });
+
+      this.current = '';
+    }
+  }
+
 
 }
